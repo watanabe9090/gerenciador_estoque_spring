7fa4ae1346d75f85610f4731dfd2e4b334a6ca13
Yuri Takae <yuriwatanabe9090@gmail.com>
2021-07-27 11:25:16 -0300

    Classe referente ao Lote

---
+++ src/main/java/com/gerenciador_estoque/model/Lote.java
@@ -0,0 +1,27 @@
+package com.gerenciador_estoque.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity @Data @Builder
+@NoArgsConstructor @AllArgsConstructor
+public class Lote {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate dataFabricacao;
+    @NotNull @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate dataVencimento;
+}
