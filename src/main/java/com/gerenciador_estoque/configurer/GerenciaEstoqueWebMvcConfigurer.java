87382d3d9534e05583a03af53f4aa905f35eace2
Yuri Takae <yuriwatanabe9090@gmail.com>
2021-07-30 10:54:28 -0300

    Configurações de Paginação

---
+++ src/main/java/com/gerenciador_estoque/configurer/GerenciaEstoqueWebMvcConfigurer.java
@@ -0,0 +1,22 @@
+package com.gerenciador_estoque.configurer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class GerenciaEstoqueWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver pageHandler = new PageableHandlerMethodArgumentResolver();
+        pageHandler.setFallbackPageable(PageRequest.of(0, 15));
+        resolvers.add(pageHandler);
+        //WebMvcConfigurer.super.addArgumentResolvers(resolvers);
+    }
+}
