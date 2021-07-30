f970db4df12cadb7e47ef719311beed3edcf693f
Yuri Takae <yuriwatanabe9090@gmail.com>
2021-07-30 10:56:44 -0300

    Handler para exceções de bad request

---
+++ src/main/java/com/gerenciador_estoque/handler/RestExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.gerenciador_estoque.handler;
+
+import com.gerenciador_estoque.exception.BadRequestException;
+import com.gerenciador_estoque.exception.BadRequestExceptionDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException badRequestException) {
+        return new ResponseEntity<>(
+                BadRequestExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Confira a documentação")
+                        .details(badRequestException.getMessage())
+                        .developerMessage(badRequestException.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+
+}
