f583985ecbf9a00a3c9480b959b2d5b0f016b216
Yuri Takae <yuriwatanabe9090@gmail.com>
2021-07-31 13:43:12 -0300

    Handler para Exceções de validação

--- src/main/java/com/gerenciador_estoque/handler/RestExceptionHandler.java
+++ src/main/java/com/gerenciador_estoque/handler/RestExceptionHandler.java
@@ -2,12 +2,17 @@ package com.gerenciador_estoque.handler;
 
 import com.gerenciador_estoque.exception.BadRequestException;
 import com.gerenciador_estoque.exception.BadRequestExceptionDetails;
+import com.gerenciador_estoque.exception.ValidationExceptionDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class RestExceptionHandler {
@@ -21,6 +26,23 @@ public class RestExceptionHandler {
                         .title("Bad Request Exception, Confira a documentação")
                         .details(badRequestException.getMessage())
                         .developerMessage(badRequestException.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
+        String fieldMessages = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(",  "));
+        return new ResponseEntity<>(
+                ValidationExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Campo Inválido")
+                        .details(exception.getMessage())
+                        .developerMessage(exception.getClass().getName())
+                        .fields(fields)
+                        .fieldsMessage(fieldMessages)
                         .build(), HttpStatus.BAD_REQUEST);
     }
 
